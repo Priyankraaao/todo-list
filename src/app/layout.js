1941ec7320bcd248dacab89daf6368e140583afb
Priyank <priyank@Priyanks-MacBook-Pro.local>
2023-09-07 17:46:32 +0530

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <HomePage children={children}/>
+        <HomePage>
+          {children}
+        </HomePage>
       </body>
     </html>
   )
--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -3,14 +3,8 @@ import React  from "react";
 import SideNavbar from "../SideNavigation";
 
 const HomePage = ({ children }) => {
-
   return (
-    <>
-    {/* <div style={{ width: "20%" }}>
-          <SideNavbar />
-        </div> */}
       <div style={{ width:"100%" }}>{children}</div>
-    </>
   );
 };
 
